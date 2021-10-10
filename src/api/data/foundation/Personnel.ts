f8a8fdca2ad27dd80b9737a8f3392fd8f59c8cf9
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-10-10 17:30:57 -0400

    init personnel data

---
+++ src/api/data/foundation/Personnel.ts
@@ -0,0 +1,26 @@
+const personnel = {
+    asheworth: {
+        name: "Dr. Daniel Asheworth",
+        about: "Thaumaturgist and self-proclaimed alchemist. Stubborn, temperamental, and sometimes arrogant, though usually useful just enough to escape disciplinary action. Born from an unknown American mother and an unknown Polish father in the late 1950s. One of few people in the Foundation capable of prolonging their youth through anomalous means." + "\n\n" + "Lead of the Damien Nowak Case. Believed to once have had been a temporary member of the Wanderers' Library and Wilson's Wildlife Solutions, Daniel Asheworth has proven numerous times to be a useful asset in Site-120's Director Council."
+    },
+    bailey: {
+        name: "Doctors Thomas, Trevor and Tristan Bailey",
+        about: "Identical triplets employed by the Foundation. Sons of Tyler Bailey, inventor of the Multi-Universal Transit Array. Employed by the Foundation for an unknown period of time (employee records and personal accounts differ), and look anywhere between 20 and 40 as a side effect of constantly crossing universal barriers." + "\n\n" + "Trevor Bailey is the former head of the Department of Multi-Universal Affairs, before being demoted to a desk job at Site-19 for mishandling of a Keter-class anomaly. Tom “Bombadill” Bailey is currently the commissioner of Site-1483 (within the Antarctic Empire), following a career spent exploring alternate universes. Tristan Bailey is a physicist, diplomat and designer of a multi-universal translator. Formerly assigned to [REDACTED], currently stationed at Site-87.",
+    },
+    bridge: {
+        name: "Dr. Django Bridge",
+        about: "Foundation Archivist. Quiet but influential, with a touch of both melancholy and whimsy. Extraordinary memory. On a first-name basis with Dr. Bright, and has frequently acted as an informal assistant for him.",
+    },
+    bright: {
+        name: "Dr. Jack Bright",
+        about: "Director of Foundation Personnel. Somewhat amoral. Extremely loyal to the Foundation. May be ridiculous, may be terrifying; is certainly blunt. Attached to SCP-963, and is therefore immortal, using the body of whatever SCP-963 has last touched.",
+    },
+    cimmerian: {
+        name: "Dr. Jeremiah Cimmerian",
+        about: "An ethics committee liaison that takes his job only somewhat seriously. He never thought his doctorate in English Literature or his minor in philosophy would actually help him stay employed, but the former got him recruited into the Foundation as a researcher and the latter secured his employment in the long term. Has an unusual interest in puns and the ethical treatment of humanoids.",
+    },
+    clef: {
+        name: "Dr. Alto Clef",
+        about: "Enigmatic and genre-savvy. A highly adaptable, clever liar. Former Global Occult Coalition (GOC) operative, specializing in reality benders. Also former file clerk. Undergone anomalous alterations that provide resistance to reality shifts and prevent his face from being photographed. Has a long and checkered history, a flair for the dramatic, and a somewhat hidden streak of self-hatred. Reformed misogynist. Most rumors about him are exaggerated or off-point."
+    }
+}
