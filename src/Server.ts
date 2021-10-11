8eb6afd6ea270b22c581d45a5dc9bc86465a853b
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-10-10 22:57:38 -0400

    init webscraper promise

--- src/Server.ts
+++ src/Server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {Application} from "./api/Application";
 import * as http from "http";
 import LogController from "./api/controllers/LogController";
+import SCPScraper from "./api/util/SCPScraper";
 
 const app = express();
 new Application(app);
@@ -11,3 +12,5 @@ const server = http.createServer(app);
 server.listen(process.env.PORT || 3000, () => {
    LogController.info("Now running on port 3000.");
 });
+
+SCPScraper.getScpData("1017").then(r => console.log(r));
--- src/api/util/SCPScraper.ts
+++ src/api/util/SCPScraper.ts
@@ -1,23 +1,40 @@
-import scraper from "scp-scraper";
+import request from "request";
+import cheerio from "cheerio";
 
 export default class SCPScraper {
 
-    public static async scrapeScp(scp: string) {
-        return await scraper(`https://www.scpwiki.com/scp-${scp}`)
-            .then(async result => {
-                const procedures = result.procedures;
-                for (let i = 0; i < procedures.length; i++) {
-                    procedures[i] = procedures[i]
-                        .replace(/█/g, 'X')
-                        .replace(/\\\\/g, "")
-                        .replace(/u2019/g, "'")
-                        .replace(/\\/g, "");
-                }
-                return {
-                    item: result.index,
-                    class: result.class,
-                    procedures: procedures
+    public static async getScpData(scp: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            request("https://www.scpwiki.com/scp-" + scp, (error, response, html) => {
+                if (!error && response.statusCode == 200) {
+                    const $ = cheerio.load(html);
+                    if (scp === "001") {
+                        const name = "SCP-" + scp;
+                        const warning = $(".content-panel").find("p").first().text()
+                            .replace(/█/g, "X");
+                        resolve({
+                            item: name,
+                            content: {
+                                warning: warning
+                            }
+                        });
+                    } else {
+                        const item = $("#page-title").first().text().replace(/\s\s+/g, "");
+                        const objectClass = $('p:contains("Object Class:")').text().replace(/█/g, 'X');
+                        const procedures = [
+                            $('p:contains("Special Containment Procedures:")').text().replace(/█/g, 'X'),
+                            $('p:contains("Special Containment Procedures:")').next().text().replace(/█/g, 'X')
+                        ];
+                        const description = $('p:contains("Description:")').text().replace(/█/g, 'X');
+                        resolve({
+                            item: item,
+                            class: objectClass,
+                            description: description,
+                            procedures: procedures
+                        });
+                    }
                 }
             });
+        });
     }
 }
