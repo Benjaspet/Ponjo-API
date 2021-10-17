9cc7ed29e4900d6609796b75414cb1914ca4e6a5
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-10-17 16:01:25 -0400

    finalized scp branch data

---
+++ src/api/data/foundation/Branches.ts
@@ -0,0 +1,70 @@
+const branches = {
+    base: {
+        logo: "https://scp-int.wdfiles.com/local--files/main/scp-logo-en-400.png",
+        about: null
+    },
+    pride: {
+        logo: "https://i.kym-cdn.com/photos/images/facebook/001/386/104/41d.png",
+        about: null
+    },
+    international: {
+        logo: "https://scp-wiki.wdfiles.com/local--files/scp-international/scp-logo-int.png",
+        about: "The International Translation Archive contains translations of SCP articles, tales, canons and other articles from all the different branches."
+    },
+    russian: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-ru-400.png",
+        about: "Фонд SCP существует и действует подпольно и тайно, сдерживая аномалии, чтобы гарантировать нормальное состояние."
+    },
+    korean: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-ko-400.png",
+        about: "SCP 재단은 정상을 보장하기 위해 변칙성을 격리하면서 은밀하고 비밀리에 존재하고 운영합니다."
+    },
+    chinese: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-cn-400.png",
+        about: "SCP基金会秘密地和秘密地存在和运作，包含异常以确保正常。"
+    },
+    french: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-fr-400.png",
+        about: "La Fondation SCP existe et opère clandestinement et secrètement, contenant des anomalies pour assurer la normalité."
+    },
+    polish: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-pl-400.png",
+        about: "Fundacja SCP istnieje i działa potajemnie i potajemnie, zawierając anomalie w celu zapewnienia normalności."
+    },
+    spanish: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-es-400.png",
+        about: "La Fundación SCP existe y opera de manera clandestina y secreta conteniendo anomalías para asegurar la normalidad."
+    },
+    thai: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-th-400.png",
+        about: "สถาบัน SCP ดำรงอยู่และดำเนินการอย่างลับๆ ซ่อนๆ โดยมีความผิดปกติเพื่อให้แน่ใจว่าเป็นเรื่องปกติ"
+    },
+    japanese: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-jp-400.png",
+        about: "SCP財団は秘密裏に存在し、秘密裏に運営されており、正常性を確保するための異常が含まれています。"
+    },
+    german: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-de-400.png",
+        about: "Die SCP Foundation existiert und operiert heimlich und heimlich und enthält Anomalien, um die Normalität zu gewährleisten."
+    },
+    italian: {
+        logo: "https://o5command-int.wdfiles.com/local--files/tech-team:graphic-templates/scp-logo-it-400.png",
+        about: "La Fondazione SCP esiste e opera in modo clandestino e segreto, contenendo anomalie per garantire la normalità."
+    },
+    ukrainian: {
+        logo: "https://scp-wiki.wdfiles.com/local--files/scp-international/scp-logo-ua-400.png",
+        about: "Фонд SCP існує і діє таємно і таємно, містить аномалії для забезпечення нормальності."
+    },
+    portuguese: {
+        logo: "https://scp-wiki.wdfiles.com/local--files/scp-international/scp-logo-pt-400.png",
+        about: "A SCP Foundation existe e opera clandestina e secretamente, contendo anomalias para garantir a normalidade."
+    },
+    czech: {
+        logo: "https://scp-int.wdfiles.com/local--files/main/scp-logo-cs-400.png",
+        about: "Nadace SCP existuje a působí tajně a tajně a obsahuje anomálie k zajištění normality."
+    },
+    traditionalChinese: {
+        logo: "https://scp-int.wdfiles.com/local--files/main/scp-logo-zh-400.png",
+        about: "SCP基金會秘密地和秘密地存在和運作，包含異常以確保正常。"
+    },
+}
